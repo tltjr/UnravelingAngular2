4fbb332b372d2c4732548a165825028c6f46fa82
Thomas Thornton <tlthornt@srpnet.com>
2016-12-15 09:43:38 -0700

    added dive log without content

--- UnravelingAngular2/ClientApp/app/app.module.ts
+++ UnravelingAngular2/ClientApp/app/app.module.ts
@@ -6,6 +6,7 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { DiveLogComponent } from './components/divelog/divelog.component';
 
 @NgModule({
     bootstrap: [ AppComponent ],
@@ -13,6 +14,7 @@ import { CounterComponent } from './components/counter/counter.component';
         AppComponent,
         NavMenuComponent,
         CounterComponent,
+        DiveLogComponent,
         FetchDataComponent,
         HomeComponent
     ],
@@ -23,6 +25,7 @@ import { CounterComponent } from './components/counter/counter.component';
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'divelog', component: DiveLogComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
---
+++ UnravelingAngular2/ClientApp/app/components/divelog/divelog.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'divelog',
+    template: require('./divelog.component.html')
+})
+export class DiveLogComponent {
+}
